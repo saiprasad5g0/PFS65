91f8a83cb8fceef812e7a804f05dccdc4272e3f2
saiprasad5g0 <saiprasadjada867@gmail.com>
2026-08-17 17:53:25 +0530

    Add files via upload

---
+++ DAY10/if.py
@@ -0,0 +1,12 @@
+'''sales = int(input("Enter the sales:"))
+if sales>1000:
+    print("best  seller") 
+
+eli_acc = eval(input("Eligible Account: "))
+ver_sub = eval(input("Meta verified subscription: "))
+if eli_acc & ver_sub:
+    print("Verified Bagde Granted")'''
+
+rain_status = eval(input("Enter the rain status :"))
+if rain_status:
+    print("Extra charges Applied")
---
+++ DAY10/ifelse.py
@@ -0,0 +1,6 @@
+'''Username = input("Username: ")
+Password = input("Password: ")
+if Username =='admin' and Password =='admin123':
+    print("Login ")
+else :
+    print("Invalid Credinals")'''
